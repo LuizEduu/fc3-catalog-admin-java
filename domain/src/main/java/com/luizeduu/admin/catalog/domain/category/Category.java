b3be61bcb7705b479c35baf5859b86b3c432326d
Luiz <l.eduardo8316@gmail.com>
2024-02-03 17:04:39 -0300

    feat: turns Category in aggregate

--- domain/src/main/java/com/luizeduu/admin/catalog/domain/category/Category.java
+++ domain/src/main/java/com/luizeduu/admin/catalog/domain/category/Category.java
@@ -1,10 +1,11 @@
 package com.luizeduu.admin.catalog.domain.category;
 
+import com.luizeduu.admin.catalog.domain.AggregateRoot;
+
 import java.time.Instant;
 import java.util.UUID;
 
-public class Category {
-	private String id;
+public class Category extends AggregateRoot<CategoryId> {
 	private String name;
 	private String description;
 	private boolean active;
@@ -13,84 +14,53 @@ public class Category {
 	private Instant deletedAt;
 
 	private Category(
-									final String id,
-									final String name,
-									final String description,
-									final boolean active,
-									final Instant createdAt,
-									final Instant updatedAt,
-									final Instant deletedAt) {
-		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.active = active;
-		this.createdAt = createdAt;
-		this.updatedAt = updatedAt;
-		this.deletedAt = deletedAt;
+									final CategoryId anId,
+									final String aName,
+									final String aDescription,
+									final boolean isActive,
+									final Instant aCreationDate,
+									final Instant aUpdateDate,
+									final Instant aDeleteDate) {
+		super(anId);
+		this.name = aName;
+		this.description = aDescription;
+		this.active = isActive;
+		this.createdAt = aCreationDate;
+		this.updatedAt = aUpdateDate;
+		this.deletedAt = aDeleteDate;
 	}
 
 	public static Category newCategory(
 		final String aName, final String aDescription, final boolean isActive
 	){
-		final var id = UUID.randomUUID().toString();
+		final var id = CategoryId.unique();
 		final var now = Instant.now();
 
 		return new Category(id, aName, aDescription, isActive, now, now, null);
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
 	}
 
 	public String getName() {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public String getDescription() {
 		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
 	}
 
 	public boolean isActive() {
 		return active;
 	}
 
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-
 	public Instant getCreatedAt() {
 		return createdAt;
-	}
-
-	public void setCreatedAt(Instant createdAt) {
-		this.createdAt = createdAt;
 	}
 
 	public Instant getUpdatedAt() {
 		return updatedAt;
 	}
 
-	public void setUpdatedAt(Instant updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
 	public Instant getDeletedAt() {
 		return deletedAt;
 	}
 
-	public void setDeletedAt(Instant deletedAt) {
-		this.deletedAt = deletedAt;
-	}
 }
